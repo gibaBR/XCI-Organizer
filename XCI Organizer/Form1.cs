008ee344db482140d1b6098b76ecd119071d6d7b
StudentBlake <6874208+StudentBlake@users.noreply.github.com>
2018-07-07 12:12:59 -0400

    Rev bump

--- XCI Organizer/Form1.cs
+++ XCI Organizer/Form1.cs
@@ -86,7 +86,7 @@ namespace XCI_Organizer {
             string assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
             string[] versionArray = assemblyVersion.Split('.');
             assemblyVersion = string.Join(".", versionArray.Take(NUMBERSINVERSION));
-            this.Text = "XCI Organizer v" + assemblyVersion + "rev003";
+            this.Text = "XCI Organizer v" + assemblyVersion + "rev004";
             bwUpdateFileList.WorkerReportsProgress = true;
 
             if (!File.Exists("keys.txt")) {
