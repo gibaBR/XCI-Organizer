eab1705ce46a8f4381813c14a47e7817490f35c3
StudentBlake <6874208+StudentBlake@users.noreply.github.com>
2018-07-08 15:13:58 -0400

    Revert "Merge branch 'master' into master"
    This reverts commit 0c0487807b68002b97da8ae055096c6868f4168f, reversing
    changes made to a4e97d3eb6cd1337c710734d9df66081ad35b681.

--- XCI Organizer/Util.cs
+++ XCI Organizer/Util.cs
@@ -6,7 +6,6 @@ using System.Linq;
 //using System.Threading.Tasks;
 using static XCI_Organizer.Form1;
 using XCI_Organizer.XTSSharp;
-using System.Xml.Linq;
 
 namespace XCI_Organizer {
     internal static class Util {
@@ -171,8 +170,7 @@ namespace XCI_Organizer {
             return array;
         }
 
-        public static FileData GetFileData(string filepath)
-        {
+        public static FileData GetFileData(string filepath) {
             FileData result = new FileData();
             //Basic Info
             result.FilePath = filepath;
